f139d3cb5051e9d54f4603c8713a36db0290e484
ewerton duarte <ewertonduarte@gmail.com>
2022-07-25 00:35:11 -0300

    ZipDocsFiles was implemented

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
-from dulbackup.util import BackupSetting
+from dulbackup.util import BackupSetting, ZipDocsBackup
 import logging
+from pathlib import Path
 
 def main():
-    SETTING_PATH = 'backup_settings.json'
+    SETTING_PATH = Path('./backup_settings.json')
+    LOG_PATH = Path('./output/log.txt')
+    
+    allowed_files = [
+        '',
+        'pdf', 
+        'txt',
+        'odt',
+        'doc',
+        'docx',
+        'csv',
+        'xls',
+        'xlsx',
+        'pot',
+        'potx',
+        'png',
+        'jpg',
+        'jpeg'
+    ]
 
-    logging.basicConfig(filename='log.txt',format='[%(levelname)s]:%(message)s\t%(asctime)s',  level=logging.DEBUG)
+    logging.basicConfig(filename=LOG_PATH,format='[%(levelname)s]:%(message)s\t%(asctime)s',  level=logging.DEBUG)
+
     logging.info('*** Dulbackup started***')
     print('[INFO]*** Dulbackup started***')
-
     
     backup_pars = BackupSetting(SETTING_PATH)
     backup_pars.load_setting()
+
+
+    zip_backup = ZipDocsBackup(Path(backup_pars.destination_folder))
+
+    zip_backup.add_dir(backup_pars.source_folders[0], allowed_files)
+
+    zip_backup.close_zipfile()
     
 if __name__ == '__main__':
     main()
--- dulbackup/util.py
+++ dulbackup/util.py
@@ -1,13 +1,18 @@
+from fileinput import filename
 from json import loads, JSONDecodeError
+from datetime import datetime
+from pathlib import Path
 import logging
+import zipfile
+import os
 
 class BackupSetting:
     def __init__(self, setting_path):
         self.setting_path = setting_path
         self.source_folders = []
-        self.destination_folders = ''
-        self.log_path = ''
-        self.schedule = ''
+        self.destination_folders = Path()
+        self.log_path = Path()
+        self.schedule = Path()
 
     def load_setting(self):
         try:
@@ -15,10 +20,10 @@ class BackupSetting:
                 json_formated_text = setting_file.read().replace('\\', '\\\\')
                 setting = loads(json_formated_text)
 
-            self.source_folders = setting['source_folders']
-            self.destination_folder = setting['destination_folder']
-            self.log_path = setting['log_path']
-            self.schedule = setting['scheduling']
+            self.source_folders = [ Path(path) for path in setting['source_folders']]
+            self.destination_folder = Path(setting['destination_folder'])
+            self.log_path = Path(setting['log_path'])
+            self.schedule = Path(setting['scheduling'])
 
         except JSONDecodeError:
             logging.error(f'{self.setting_path} file is not valid Json')
@@ -38,5 +43,39 @@ class BackupSetting:
 
             exit()
 
-class ZipBackup:
-    pass
+class ZipDocsBackup:
+    def __init__(self, zip_src):
+        filename = zip_src / f"backup_{datetime.today().strftime('%Y-%m-%d')}.zip"
+
+        self.zip_obj = zipfile.ZipFile(
+            filename,
+            'w',
+            zipfile.ZIP_DEFLATED
+        )
+
+    def add_dir(self, dir_path, allowed_files):
+        suffixes = [f'.{sufix}' for sufix in allowed_files]
+
+        try:
+            for root, _, filenames in os.walk(dir_path):
+                for filename in filenames:
+                    filename = Path(filename)
+                    if filename.suffix in suffixes:
+                        absolute_path_file = root / filename
+                        
+                        self.zip_obj.write(
+                            absolute_path_file,
+                            absolute_path_file.relative_to(dir_path.parent)
+                        )
+                        
+        except FileNotFoundError:
+            logging.error(f'{self.setting_path} wasnt found in add_dir from ZipDocsFile Object')
+            logging.info('Ending script')
+
+            print(f'[ERROR]:{self.setting_path} wasnt found')
+            print('[INFO]:Ending script')
+
+            exit()
+
+    def close_zipfile(self):
+        self.zip_obj.close()
